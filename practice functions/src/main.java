334f853da1c4d81334c761055321e6a72115d7e7
Shinzuu <not.shinzuu@gmail.com>
2023-12-05 20:04:58 +0600

    practice

---
+++ practice functions/src/main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		int t = scn.nextInt();
+		scn.nextLine();
+		System.out.print(t);
+		String s = scn.nextLine();
+		System.out.println(s);
+		String s1 = scn.nextLine();
+		System.out.println(s1);
+	}
+
+}
